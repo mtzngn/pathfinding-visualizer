419ae56f51e93792e389a9bc364b70ff3ad7abc8
Taha <mt.zngn@gmail>
2021-03-24 11:07:36 +0000

    navbar created with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavbarComponenet from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Algo</h1>
+      <NavbarComponenet/>
+      <div className="info"><h1>info</h1></div>
+      <div className="visualizer"></div>
     </div>
   );
 }
