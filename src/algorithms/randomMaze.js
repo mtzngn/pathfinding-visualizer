78054eb7f73f956748c9bc6e658af328bcd0bf92
Taha <mt.zngn@gmail>
2021-04-17 17:01:22 +0100

    poor and not working as intended random maze algorithm

--- src/algorithms/randomMaze.js
+++ src/algorithms/randomMaze.js
@@ -1,21 +1,37 @@
 const randomMaze = (nodes) => {
 
-    let stack = [];
-    let walls = [];
-    stack.push(nodes[0]);
+    let stack = [nodes[0]];
+    let count = 0;
+    while (count !== nodes.length) {
+        let items = []
 
-    while (stack.length > 0) {
+        for (const node of nodes) {
+            let north = (node.x === stack[0].x && node.y === stack[0].y - 1)
+            let south = (node.x === stack[0].x && node.y === stack[0].y + 1)
+            let east = (node.x === stack[0].x - 1 && node.y === stack[0].y)
+            let west = (node.x === stack[0].x + 1 && node.y === stack[0].y)
 
-        for (let node of nodes) {
-            let north = (node.x === stack[stack.length - 1].x && node.y === stack[stack.length - 1].y - 1)
-            let south = (node.x === stack[stack.length - 1].x && node.y === stack[stack.length - 1].y + 1)
-            let east = (node.x === stack[stack.length - 1].x - 1 && node.y === stack[stack.length - 1].y)
-            let west = (node.x === stack[stack.length - 1].x + 1 && node.y === stack[stack.length - 1].y)
-            if (north || south || east || west) {
-
+            if ((north || south || east || west) && (node.wall === false) && (node.visited === false)) {
+                items.push(node)
             }
         }
+        if (items.length === 0) {
+            if (Math.round(Math.random()) === 0) {
+                if(!stack[0].start && !stack[0].end) stack[0].wall = true;
+            } 
+            stack.shift();
+            continue;
+        }
+        let currentNode = items[Math.floor(Math.random() * items.length)];
+        if(currentNode.visted === true) {
+            stack.shift();
+            continue;
+        }
+        currentNode.visited = true;
+        count++;
+        stack.unshift(currentNode);
     }
+    return nodes;
 }
 
 export default randomMaze;
