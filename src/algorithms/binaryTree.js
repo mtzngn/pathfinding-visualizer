2929ac4d773edab5f155d763b20ac3ea3c27529d
Taha <mt.zngn@gmail>
2021-04-25 18:10:59 +0100

    random maze completed

--- src/algorithms/binaryTree.js
+++ src/algorithms/binaryTree.js
@@ -2,7 +2,6 @@ const binaryTree = (nodes) => {
     nodes.forEach((node) => {
         if (!(node.start || node.end) && (node.y % 2 === 0 || node.x % 2 === 0) && node.y !== 30 && node.x !== 0) node.wall = true;
     })
-
     nodes.forEach((node) => {
         if (node.y % 2 !== 0 && node.x % 2 !== 0) {
             let ranNum = Math.floor(Math.random() * 2)
@@ -16,7 +15,6 @@ const binaryTree = (nodes) => {
                 if(node2.x === node.x + 1 && node2.y === node.y ) node2.wall = false
             });
         }
-
         }
     })
 
--- src/algorithms/randomMaze.js
+++ src/algorithms/randomMaze.js
@@ -1,4 +1,4 @@
-const randomMaze2 = (nodes) => {
+const randomMaze = (nodes) => {
     let sideWalls = nodes.filter(node => (node.x === 0 || node.y % 2=== 0 || node.x === 47 || node.y === 30))
     for (let i = 2 ; i < 30; i += 2) {
         let numberx = Math.floor( Math.random() * 44 / 2 ) * 2 + 2;
@@ -7,4 +7,4 @@ const randomMaze2 = (nodes) => {
     sideWalls.forEach((node) => node.wall = true)
     return sideWalls
 }
-export default randomMaze2;
+export default randomMaze;
